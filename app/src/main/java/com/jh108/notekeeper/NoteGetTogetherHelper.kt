183b6f6cab50398d542406cd080edce843e8c293
Jesse Hill <jesse_hill@eogresources.com>
2020-11-13 12:23:03 -0600

    Add lifecycle hooks to connect to the messaging and location system

--- app/src/main/java/com/jh108/notekeeper/NoteGetTogetherHelper.kt
+++ app/src/main/java/com/jh108/notekeeper/NoteGetTogetherHelper.kt
@@ -21,13 +21,42 @@ class NoteGetTogetherHelper(val context: Context, val lifecycle: Lifecycle) : Li
         Log.d(tag, "Location Callback Lat:${lat}, Lon:${lon}")
     }
 
+    val msgManager = PseudoMessagingManager(context)
+    var msgConnection: PseudoMessagingConnection? = null
+
+    fun sendMessage(note: NoteInfo) {
+        val getTogetherMessage = "$currentLat|$currentLon|${note.title}|${note.course?.title}"
+        msgConnection?.send(getTogetherMessage)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun connectMsgManager() {
+        Log.d(tag, "onCreate")
+        msgManager.connect { connection ->
+            Log.d(tag, "Connection Callback")
+            if (lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED))
+                msgConnection = connection
+            else
+                connection.disconnect()
+
+        }
+    }
+
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun startHandler() {
+        Log.d(tag, "startHandler")
         locManager.start()
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun stopHandler() {
+        Log.d(tag, "stopHandler")
         locManager.stop()
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun disconnectMsgManager() {
+        Log.d(tag, "Disconnect")
+        msgConnection?.disconnect()
     }
 }
